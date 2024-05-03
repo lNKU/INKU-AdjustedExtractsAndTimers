83f373be0a9469ad1aa21016ac7116a66c3408ec
wcarpen <wcarpen4@gmail.com>
2024-05-03 02:20:14 -0400

    remove extra output log statement

--- src/mod.ts
+++ src/mod.ts
@@ -77,7 +77,6 @@ class AdjustedExtractsAndTimers implements IPostDBLoadMod, IPreAkiLoadMod {
                             
                             if (!this.excludedMaps.includes(mapName.toLowerCase())) {
                                 this.adjustMapData(mapName, this.locationsDb);
-                                // this.logger.warning(`[${this.modName}]: ${displayedMapName.toUpperCase()} extract timers and requirements re-randomized.`)
                                 
                             }
                         }
